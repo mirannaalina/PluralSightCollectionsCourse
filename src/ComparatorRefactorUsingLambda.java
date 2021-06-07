fa9a215e606fdbb4ea7d9e8442d92acf122b7298
mirannaalina <v_mxa@yahoo.com>
2021-06-07 19:06:58 +0300

    cap 1 half

--- src/ComparatorRefactorUsingLambda.java
+++ src/ComparatorRefactorUsingLambda.java
@@ -1,2 +1,37 @@
+import java.util.Comparator;
+
 public class ComparatorRefactorUsingLambda {
+
+    public static void main(String[] args) {
+
+        //Comparator
+
+        Comparator<String> comparator = new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return Integer.compare(o1.length(),o2.length());
+            }
+        };
+
+        //becomes with lambda :
+
+
+        //for body {} needs a return statement
+        Comparator<String> comparator1 = (String o1, String o2) -> {
+            return Integer.compare(o1.length(), o2.length());
+        };
+
+        Comparator<String> comparator2 = (String o1, String o2) ->
+            Integer.compare(o1.length(), o2.length());
+
+
+        String st1 = "String11111";
+        String st2 = "String2111111";
+
+        System.out.println(comparator.compare(st1,st2));
+        System.out.println(comparator1.compare(st1,st2));
+
+
+
+    }
 }
---
+++ src/MethodRefEx.java
@@ -0,0 +1,28 @@
+import java.util.function.BinaryOperator;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class MethodRefEx {
+
+    public static void main(String[] args) {
+
+        Function<Person, Integer> f = person -> person.getAge();
+
+        Function<Person, Integer> f1 = Person::getAge;
+
+        BinaryOperator<Integer> sum = (i1,i2) -> i1 +i2;
+
+        BinaryOperator<Integer> sum1 = (i1,i2) -> Integer.sum(i1,i2);
+
+        BinaryOperator<Integer> sum2 = Integer::sum;
+
+        Consumer<String> printer = s -> System.out.println(s);
+
+        Consumer<String> printer1 = System.out::println;
+
+
+
+
+
+    }
+}

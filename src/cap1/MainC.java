440b80aa0fb9a495087b86f896e23330ca4494a2
mirannaalina <v_mxa@yahoo.com>
2021-06-08 13:11:59 +0300

    cap 1 all

--- src/cap1/MainC.java
+++ src/cap1/MainC.java
@@ -15,7 +15,13 @@ public class MainC {
         Function<Person, String> f2 = p -> p.getLastName();
         Function<Person, String> f3 = p -> p.getFirstName();
 
-        Comparator<Person> cmpPerson = Comparator.comparing(p-> p.getAge());
+        Comparator<Person> cmpPersonAge = Comparator.comparing(Person::getAge);
+        Comparator<Person> cmpPersonLastName = Comparator.comparing(Person::getLastName);
+
+
+//        Comparator<Person> cmp = cmpPersonAge.comparing(Person::getLastName)
+//                .thenComparing(Person::getFirstName);
+
 
 
     }
